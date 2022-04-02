414f8d5f2437a1a0b10213f20a24a0f703ecd5fd
Alex Chukanov <alex.chukanov@gmail.com>
2022-04-02 11:43:51 +0300

    added task descripton

--- NearestVehiclePositionsApp/Program.cs
+++ NearestVehiclePositionsApp/Program.cs
@@ -8,6 +8,35 @@ using System.Threading.Tasks;
 
 using NearestVehiclePositions.Model;
 
+/*
+Your task is to write a program that can find the nearest vehicle position in the data file to each of the 10 co-ordinates 
+provided below. In addition to being able to do this, however, your program must be able to complete all 10 lookups in less 
+time than our benchmark. This benchmark is based on simply looping through each of the 2 million positions and keeping the 
+closest to each given co-ordinate. This is simply repeated for each of the 10 provided co-ordinates.
+
+Telematics telemetry result:
+Data file read execution time : 172 ms
+Closest position calculation execution time : 3917 ms
+Total execution time : 4089 ms
+
+My telemetry result:
+Load file time: 2979 ms
+Calc ten positions time: 656 ms
+Total calc time: 3635 ms
+
+The nearest cars for ten positions:
+position = 1, the nearest vehicle ID = 1017652, Latitude= 34,5450325012207, Longitude = -102,10150909423828
+position = 2, the nearest vehicle ID = 609817, Latitude= 32,34754180908203, Longitude = -99,12191009521484
+position = 3, the nearest vehicle ID = 591880, Latitude= 33,23557662963867, Longitude = -99,12040710449219
+position = 4, the nearest vehicle ID = 284382, Latitude= 35,19633102416992, Longitude = -95,34922790527344
+position = 5, the nearest vehicle ID = 1724293, Latitude= 31,895187377929688, Longitude = -97,78909301757812
+position = 6, the nearest vehicle ID = 1442878, Latitude= 32,89590835571289, Longitude = -101,78733825683594
+position = 7, the nearest vehicle ID = 108054, Latitude= 34,11565399169922, Longitude = -100,22537231445312
+position = 8, the nearest vehicle ID = 1247417, Latitude= 32,33456802368164, Longitude = -99,9914779663086
+position = 9, the nearest vehicle ID = 87700, Latitude= 33,53670883178711, Longitude = -94,79186248779297
+position = 10, the nearest vehicle ID = 282385, Latitude= 32,23622512817383, Longitude = -100,22180938720703
+*/
+
 namespace NearestVehiclePositions
 {
     internal class Program
